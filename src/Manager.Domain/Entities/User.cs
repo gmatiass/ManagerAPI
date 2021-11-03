2ff4b72f328bfac7be30b42e9a55cf99b9e9f7ac
Gabriel Matias <gmatias1793@gmail.com>
2021-11-03 20:13:46 -0300

    Domain layer

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,63 @@
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        //Propriedades
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        //EF
+        protected User() { }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+
+            Validate();
+        }
+
+        //Comportamentos
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        //Auto Validação
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+
+                throw new Exception("Invalid fields." + _errors[0]);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Validators
+{
+    class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("Entity should not be empty.")
+
+                .NotNull()
+                .WithMessage("Entity should not be null.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name should not be empty.")
+
+                .NotNull()
+                .WithMessage("Name should not be null.")
+
+                .MinimumLength(3)
+                .WithMessage("Name should have at least 3 characters.")
+
+                .MaximumLength(80)
+                .WithMessage("Name must have a maximum of 80 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email should not be empty.")
+
+                .NotNull()
+                .WithMessage("Email should not be null.")
+
+                .MinimumLength(8)
+                .WithMessage("Email should have at least 8 characters.")
+
+                .MaximumLength(180)
+                .WithMessage("Email must have a maximum of 180 characters.")
+
+                .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+                .WithMessage("Email is not valid.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password should not be empty.")
+
+                .NotNull()
+                .WithMessage("Password should not be null.")
+
+                .MinimumLength(6)
+                .WithMessage("Password should have at least 6 characters.")
+
+                .MaximumLength(30)
+                .WithMessage("Password must have a maximum of 30 characters.");
+        }
+    }
+}
